37dbd53e1f00aca91b3acd3cab5ac0d45747cf13
Ajdin Husetovic <ahusetovic1@gmail.com>
2024-06-10 15:08:57 +0200

    finish ui

---
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,16 @@
+import SearchInput from "./SearchInput";
+import Conversations from "./Conversations";
+import Logout from "./Logout";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <SearchInput />
+      <div className="divider px-3"></div>
+      <Conversations />
+      <Logout />
+    </div>
+  );
+};
+
+export default Sidebar;
